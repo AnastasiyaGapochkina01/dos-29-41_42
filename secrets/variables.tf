f45f40bf020f71f0b8c8463b2d7881ce3eccfa03
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-10-22 06:44:22 +0300

    Create variables.tf

---
+++ secrets/variables.tf
@@ -0,0 +1,15 @@
+variable "default_disk_size" {
+  default = "20"
+}
+
+variable "default_image" {
+  default = "fd82pkek8uu0ejjkh4vn"
+}
+
+variable "default_cpu" {
+  default = 2
+}
+
+variable "default_mem" {
+  default = 2
+}
