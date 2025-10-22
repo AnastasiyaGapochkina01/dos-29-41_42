5cb7aab4c49098632f3b788610d2a8640ba609e6
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-10-22 07:59:45 +0300

    Create main.tf

---
+++ vm-module/modules/yc-vm/main.tf
@@ -0,0 +1,26 @@
+resource "yandex_compute_instance" "vm" {
+  name        = var.vm_name
+  platform_id = "standard-v3"
+  zone        = var.zone
+
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.disk_size
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = var.enable_nat
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
